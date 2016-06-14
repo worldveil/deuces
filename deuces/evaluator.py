cd3c9d4dc74566f8771cdb3be9ba3aeb0a6d41e8
Will Drevo <will.drevo@gmail.com>
2016-06-14 01:14:12 -0700

    Fixes #9 issue with human string output on SF/quads boundary

--- deuces/evaluator.py
+++ deuces/evaluator.py
@@ -92,7 +92,7 @@ class Evaluator(object):
         Returns the class of hand given the hand hand_rank
         returned from evaluate. 
         """
-        if hr >= 0 and hr < LookupTable.MAX_STRAIGHT_FLUSH:
+        if hr >= 0 and hr <= LookupTable.MAX_STRAIGHT_FLUSH:
             return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_STRAIGHT_FLUSH]
         elif hr <= LookupTable.MAX_FOUR_OF_A_KIND:
             return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_FOUR_OF_A_KIND]
