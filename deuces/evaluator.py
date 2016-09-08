eb100643fae7d7d9288ee8147c60228094e03257
Will Drevo <worldveil@users.noreply.github.com>
2016-09-07 23:39:06 -0700

    Updated link

--- deuces/evaluator.py
+++ deuces/evaluator.py
@@ -6,7 +6,7 @@ from lookup import LookupTable
 class Evaluator(object):
     """
     Evaluates hand strengths using a variant of Cactus Kev's algorithm:
-    http://www.suffecool.net/poker/evaluator.html
+    http://suffe.cool/poker/evaluator.html
 
     I make considerable optimizations in terms of speed and memory usage, 
     in fact the lookup table generation can be done in under a second and 
