5c6750abb851cd366311b96e4bb466eeaf4c9f52
Zhenbo Li <litimetal@gmail.com>
2017-02-23 21:02:00 +0800

    Port from python2 to 3

--- deuces/card.py
+++ deuces/card.py
@@ -27,11 +27,11 @@ class Card ():
 
     # the basics
     STR_RANKS = '23456789TJQKA'
-    INT_RANKS = range(13)
+    INT_RANKS = list(range(13))
     PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
 
     # converstion from string => int
-    CHAR_RANK_TO_INT_RANK = dict(zip(list(STR_RANKS), INT_RANKS))
+    CHAR_RANK_TO_INT_RANK = dict(list(zip(list(STR_RANKS), INT_RANKS)))
     CHAR_SUIT_TO_INT_SUIT = {
         's' : 1, # spades
         'h' : 2, # hearts
@@ -42,10 +42,10 @@ class Card ():
 
     # for pretty printing
     PRETTY_SUITS = {
-        1 : u"\u2660".encode('utf-8'), # spades
-        2 : u"\u2764".encode('utf-8'), # hearts
-        4 : u"\u2666".encode('utf-8'), # diamonds
-        8 : u"\u2663".encode('utf-8') # clubs
+        1 : "\u2660", # spades
+        2 : "\u2764", # hearts
+        4 : "\u2666", # diamonds
+        8 : "\u2663" # clubs
     }
 
      # hearts and diamonds
@@ -189,14 +189,14 @@ class Card ():
 
         r = Card.STR_RANKS[rank_int]
 
-        return " [ " +r+ " " +s+ " ] "
+        return " [ " + r + " " + s + " ] "
 
     @staticmethod
     def print_pretty_card(card_int):
         """
         Expects a single integer as input
         """
-        print Card.int_to_pretty_str(card_int)
+        print((Card.int_to_pretty_str(card_int)))
 
     @staticmethod
     def print_pretty_cards(card_ints):
@@ -211,4 +211,4 @@ class Card ():
             else:
                 output += Card.int_to_pretty_str(c) + " "
     
-        print output
+        print(output)
--- performance/perf_deuces.py
+++ performance/perf_deuces.py
@@ -27,9 +27,9 @@ for i in range(len(boards)):
     cumtime += (time.time() - start)
 
 avg = float(cumtime / n)
-print "7 card evaluation:"
-print "[*] Deuces: Average time per evaluation: %f" % avg
-print "[*] Decues: Evaluations per second = %f" % (1.0 / avg)
+print("7 card evaluation:")
+print("[*] Deuces: Average time per evaluation: %f" % avg)
+print("[*] Decues: Evaluations per second = %f" % (1.0 / avg))
 
 ###
 
@@ -41,9 +41,9 @@ for i in range(len(boards)):
     cumtime += (time.time() - start)
 
 avg = float(cumtime / n)
-print "6 card evaluation:"
-print "[*] Deuces: Average time per evaluation: %f" % avg
-print "[*] Decues: Evaluations per second = %f" % (1.0 / avg)
+print("6 card evaluation:")
+print("[*] Deuces: Average time per evaluation: %f" % avg)
+print("[*] Decues: Evaluations per second = %f" % (1.0 / avg))
 
 ###
 
@@ -55,6 +55,6 @@ for i in range(len(boards)):
     cumtime += (time.time() - start)
 
 avg = float(cumtime / n)
-print "5 card evaluation:"
-print "[*] Deuces: Average time per evaluation: %f" % avg
-print "[*] Decues: Evaluations per second = %f" % (1.0 / avg)
+print("5 card evaluation:")
+print("[*] Deuces: Average time per evaluation: %f" % avg)
+print("[*] Decues: Evaluations per second = %f" % (1.0 / avg))
