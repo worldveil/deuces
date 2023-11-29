348b8bd2f170f294a7f1307ef6267e022a1bb7ba
Will Drevo <will@ip-172-64-56-42.us-west-2.compute.internal>
2023-11-29 00:17:48 +0000

    Bump version again

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='deuces',
-    version='0.2',
+    version='0.2.1',
     description=__doc__,
     long_description=open('README.md').read(),
     author='Will Drevo',
